1c4d6626481e78a7ca6edacfd29be1fe0acf6769
oskaddddd <mikelionisoskaras@gmail.com>
2022-05-12 19:43:10 +0300

    move

--- Mom vs Dad Unity/Assets/Scripts/Movement.cs
+++ Mom vs Dad Unity/Assets/Scripts/Movement.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    float X;
+    Rigidbody rigidbody;
+    Vector3 move;
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        X = Input.GetAxis("Horizontal");
+        move = new Vector3(X, 0, 0);
+        rigidbody.AddForce(move * speed * Time.deltaTime);
+
     }
 }
