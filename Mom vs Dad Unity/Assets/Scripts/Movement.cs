460d58da6d9601898208b10915726ec607e3f8c8
oskaddddd <mikelionisoskaras@gmail.com>
2022-05-12 21:21:39 +0300

    Did alot
    move, ground check, jump

--- Mom vs Dad Unity/Assets/Scripts/Movement.cs
+++ Mom vs Dad Unity/Assets/Scripts/Movement.cs
@@ -5,21 +5,49 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     float X;
-    Rigidbody rigidbody;
+    Rigidbody rb;
     Vector3 move;
     public float speed;
+
+    public Transform GroundCheckTransform;
+    public float GroundCheckSize;
+    RaycastHit hit;
+    public LayerMask GroundMask;
+
+    bool isGrounded = true;
+    public float JumpForce;
     // Start is called before the first frame update
     void Start()
     {
-        rigidbody = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //MOVE
         X = Input.GetAxis("Horizontal");
         move = new Vector3(X, 0f, 0f);
-        rigidbody.velocity = move * speed * Time.deltaTime;
+        rb.AddForce(move*speed*Time.deltaTime);
 
+        //GROUND CHECK 
+        if(Physics.Raycast(transform.position, new Vector3(0, -1, 0) * GroundCheckSize, out hit, GroundMask))
+        {
+            isGrounded = false;
+            Debug.Log("Did Hit");
+            Debug.DrawRay(transform.position, new Vector3(0, -1, 0) * GroundCheckSize, Color.green);
+        }
+        else
+        {
+            isGrounded = false;
+            Debug.Log("Didn't Hit");
+        }
+
+        //JUMP
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            Vector3 JumpDirection = new Vector3(0, 1, 0);
+            rb.AddForce(JumpDirection * JumpForce);
+        }
     }
 }
