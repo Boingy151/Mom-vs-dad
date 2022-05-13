2deafeca7b2fadb4af81aa3b7fc3065e0474772d
oskaddddd <mikelionisoskaras@gmail.com>
2022-05-13 21:12:06 +0300

    GrassMove And remove

--- Mom vs Dad Unity/Assets/Scripts/GrassScript.cs
+++ Mom vs Dad Unity/Assets/Scripts/GrassScript.cs
@@ -4,11 +4,24 @@ using UnityEngine;
 
 public class GrassScript : MonoBehaviour
 {
-   
 
+    Rigidbody rb;
+    public float speed;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        rb.velocity = new Vector3(0, 0, -1*speed*Time.deltaTime);
+
+        //remove
+        if(transform.position.z < -1f)
+        {
+            Destroy(gameObject);
+        }
+
     }
 }
