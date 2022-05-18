5ae33f47c895311d03febc810edc7a6a016fb394
oskaddddd <105368582+oskaddddd@users.noreply.github.com>
2022-05-18 13:34:27 +0200

    death win reset

--- Mom vs Dad Unity/Assets/Scripts/PointScript.cs
+++ Mom vs Dad Unity/Assets/Scripts/PointScript.cs
@@ -2,16 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PointScript : MonoBehaviour
 {
     public int numOfPoints = 0;
     public Material skybox;
     public Color color;
+    public GameObject Win;
+    public GameObject Lose;
+    public GameObject reset;
     void Start()
     {
-        
 
+        Time.timeScale = 1;
 
     }
 
@@ -34,7 +38,25 @@ public class PointScript : MonoBehaviour
             color = new Color(1, 1 - (0.009f * numOfPoints * -1), 0);
         }
 
+        if(numOfPoints > 99)
+        {
+            Win.SetActive(true);
+            Time.timeScale = 0;
+            reset.SetActive(true);
+        }
+        if (numOfPoints < -9)
+        {
+            Lose.SetActive(true);
+            Time.timeScale = 0;
+            reset.SetActive(true);
+        }
+
+        
+
 
     }
-    
+    public void ResetScene()
+    {
+        SceneManager.LoadScene(0);
+    }
 }
